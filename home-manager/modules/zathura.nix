92a8072157191e2e83c41fae7d16de3bf9f5ccfd
Simon Yde <git@simonyde.com>
2023-10-18 15:30:34 +0200

    changed zathura to remove external fetch
    Former-commit-id: 4f94ef19766bc434595b23644297e047c8144fbc

--- home-manager/modules/zathura.nix
+++ home-manager/modules/zathura.nix
@@ -1,13 +1,49 @@
-{ pkgs, ... }:
+{ config, ... }:
+
+let
+  catppuccin = config.themes.catppuccin;
+  flavour = config.themes.flavour;
+in
 {
   programs.zathura = {
-    extraConfig = ''
-      include ${pkgs.fetchFromGitHub {
-            owner = "catppuccin";
-            repo = "zathura";
-            rev = "d85d875";
-            sha256 = "sha256-5Vh2bVabuBluVCJm9vfdnjnk32CtsK7wGIWM5+XnacM=";
-          } + "/src/catppuccin-mocha"}
-    '';
+    options = with catppuccin.${flavour}; {
+      default-fg = text;
+      default-bg = base;
+
+      completion-bg = surface0;
+      completion-fg = text;
+      completion-highlight-bg = surface2;
+      completion-highlight-fg = text;
+      completion-group-bg = surface1;
+      completion-group-fg = blue;
+
+      statusbar-bg = surface0;
+      statusbar-fg = text;
+
+      notification-bg = surface0;
+      notification-fg = text;
+      notification-error-bg = surface0;
+      notification-error-fg = red;
+      notification-warning-bg = surface0;
+      notification-warning-fg = yellow;
+
+      inputbar-bg = surface0;
+      inputbar-fg = text;
+
+      recolor-lightcolor = base;
+      recolor-darkcolor = text;
+
+      index-bg = base;
+      index-fg = text;
+      index-active-bg = surface0;
+      index-active-fg = text;
+
+      render-loading-bg = base;
+      render-loading-fg = text;
+
+      highlight-color = surface2;
+      highlight-active-color = rosewater;
+      highlight-fg = rosewater;
+    };
   };
 }
