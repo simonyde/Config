e1b6375ede715b93cfe0b339addc4a5f73413d20
Simon Yde <git@simonyde.com>
2023-10-29 17:48:06 +0100

    Gammastep manual dusk/dawn times
    Former-commit-id: 182c4815ddfd937daaf8b6c3838bcb6db283d666

--- home-manager/modules/services/gammastep.nix
+++ home-manager/modules/services/gammastep.nix
@@ -6,6 +6,8 @@
       day   = 6500;
       night = 1400;
     };
+    duskTime = "18:45-20:30";
+    dawnTime = "6:00-7:45";
     # provider  = "geoclue2";
     latitude  = 56.3;
     longitude = 9.5;
