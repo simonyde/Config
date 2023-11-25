d5bc4e8856bd81715bb889b9668618038c5fd107
Simon Yde <git@simonyde.com>
2023-11-25 18:38:07 +0100

    package(pix2tex): Created pix2tex python package
    Former-commit-id: f0fc3930636cbb414bceb535cefb6fedd3509c91

---
+++ home-manager/packages/pix2tex/default.nix
@@ -0,0 +1,71 @@
+{ lib
+, python3
+, fetchFromGitHub
+, callPackage
+}:
+
+let
+  x-transformers = callPackage ./x-transformers.nix { };
+  timm = callPackage ./timm.nix { };
+in
+
+python3.pkgs.buildPythonApplication rec {
+  pname = "pix2tex";
+  version = "0.0.31";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "lukas-blecher";
+    repo = "LaTeX-OCR";
+    rev = version;
+    hash = "sha256-VS7zbrXRrX2nb51zGdhnydZyWuX+yZ6ssU8ZviE9N+Y=";
+  };
+
+  propagatedBuildInputs = with python3.pkgs; [
+    setuptools
+    wheel
+    torch
+    torchvision
+    pandas
+    munch
+    transformers
+    x-transformers
+    timm
+    einops
+    albumentations
+    tqdm
+    requests
+    tokenizers
+    numpy
+    pyyaml
+    pillow
+    opencv4
+
+    # GUI
+    pyqt6
+    pyqt6-webengine
+    pynput
+    screeninfo
+    pyside6
+  ];
+
+  patchPhase = ''
+    substituteInPlace ./pix2tex/model/checkpoints/get_latest_checkpoint.py \
+      --replace "path = os.path.dirname(__file__)" $'path = os.path.join(os.environ["HOME"], ".cache", "pix2tex")\n    if not os.path.exists(path):\n        os.makedirs(path)'
+    substituteInPlace ./pix2tex/__main__.py \
+      --replace "default='checkpoints/weights.pth'" "default=os.path.join(os.environ['HOME'], '.cache', 'pix2tex', 'weights.pth')" \
+      --replace $'    import os\n' "" \
+      --replace "def main():" $'def main():\n    import os\n'
+  '';
+
+
+  pythonImportsCheck = [ "pix2tex" ];
+
+  meta = with lib; {
+    description = "Pix2tex: Using a ViT to convert images of equations into LaTeX code";
+    homepage = "https://github.com/lukas-blecher/LaTeX-OCR";
+    license = licenses.mit;
+    maintainers = with maintainers; [ ];
+    mainProgram = "pix2tex";
+  };
+}
---
+++ home-manager/packages/pix2tex/entmax.nix
@@ -0,0 +1,34 @@
+{ lib
+, python3
+, fetchPypi
+}:
+
+python3.pkgs.buildPythonApplication rec {
+  pname = "entmax";
+  version = "1.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-HNlyHDSTWCTgccjCCQBxHK1gw9hLyeRmjzt6iHO/Ys8=";
+  };
+
+  nativeBuildInputs = [
+    python3.pkgs.setuptools
+    python3.pkgs.wheel
+  ];
+
+  propagatedBuildInputs = with python3.pkgs; [
+    torch
+  ];
+
+  pythonImportsCheck = [ "entmax" ];
+
+  meta = with lib; {
+    description = "The entmax mapping and its loss, a family of sparse alternatives to softmax";
+    homepage = "https://pypi.org/project/entmax/";
+    license = licenses.mit;
+    maintainers = with maintainers; [ ];
+    mainProgram = "entmax";
+  };
+}
