70dda4513ccc2350ea873ee1a928565775901e35
Simon Yde <git@simonyde.com>
2023-12-21 20:34:34 +0100

    feat(home-manager): dunst
    Former-commit-id: b9a9b54419e059d284bce98f2a9d505fb89effb1

---
+++ home-manager/modules/services/dunst.nix
@@ -0,0 +1,37 @@
+{ config, ... }:
+
+let
+  terminal = config.syde.terminal;
+  colors = config.colorScheme.colors;
+in
+{
+  services.dunst = {
+    settings = with colors; {
+      global = {
+        frame_color = "#${base07}";
+        font = "${terminal.font} 10";
+        frame_width = 2;
+        separator_color = "frame";
+        corner_radius = 10;
+        icon_position = "left";
+        show_indicator = true;
+      };
+
+      urgency_low = {
+        background = "#${base00}";
+        foreground = "#${base05}";
+      };
+
+      urgency_normal = {
+        background = "#${base00}";
+        foreground = "#${base05}";
+      };
+
+      urgency_critical = {
+        background = "#${base00}";
+        foreground = "#${base05}";
+        frame_color = "#${base08}";
+      };
+    };
+  };
+}
