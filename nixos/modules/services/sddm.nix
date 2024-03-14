ee1f269530a70140622ecba00e8dc5c958593f1f
Simon Yde <git@simonyde.com>
2024-03-14 14:03:54 +0100

    NixOS perdix: 191 current  2024-03-14 13:58:44  24.05.20240303.b8697e5  6.6.19                          *
    Former-commit-id: 429af32884afe52c94b604b56e1082ed14ae7256

--- nixos/modules/services/sddm.nix
+++ nixos/modules/services/sddm.nix
@@ -5,7 +5,7 @@ let cfg = config.services.xserver.displayManager.sddm; in
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       # inputs.sddm-catppuccin.packages.${pkgs.hostPlatform.system}.sddm-catppuccin
-      (pkgs.callPackage ../../packages/catppuccin-sddm.nix {}).catppuccin-sddm
+      (pkgs.callPackage ../../packages/catppuccin-sddm.nix {})
       libsForQt5.qt5.qtgraphicaleffects
       libsForQt5.qt5.qtsvg
       libsForQt5.qt5.qtquickcontrols2
