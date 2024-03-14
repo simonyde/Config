522efd51f111355375962178d23ff93921df0b48
Simon Yde <git@simonyde.com>
2024-03-14 12:14:47 +0100

    NixOS perdix: 187 current  2024-03-14 12:14:44  24.05.20240303.b8697e5  6.6.19                          *
    Former-commit-id: cae57f8cf210daf5ee81f80f81b1bb93cc414b0c

--- nixos/modules/services/sddm.nix
+++ nixos/modules/services/sddm.nix
@@ -1,8 +1,12 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 let cfg = config.services.xserver.displayManager.sddm; in
 {
   config = lib.mkIf cfg.enable {
+    environment.systemPackages = [
+      inputs.sddm-catppuccin.packages.${pkgs.hostPlatform.system}.sddm-catppuccin
+    ];
+
     services.xserver.displayManager = {
       autoLogin = {
         user = "syde";
@@ -12,6 +16,7 @@ let cfg = config.services.xserver.displayManager.sddm; in
         wayland.enable =
           # TODO: add more wayland WM/DE options
           config.programs.sway.enable || config.programs.hyprland.enable;
+        theme = "catppuccin";
       };
     };
   };
