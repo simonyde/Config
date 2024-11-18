ce74155fb6946250e35111c233956210c6ff7fe9
Simon Yde <git@simonyde.com>
2024-11-18 22:19:36 +0100

    feat(wireguard): update certs
    Former-commit-id: 9c1cea562d51f5b949d161427285c195169296c5

--- nixos/modules/services/wireguard.nix
+++ nixos/modules/services/wireguard.nix
@@ -7,16 +7,16 @@
 {
   config = lib.mkIf config.networking.wireguard.enable {
     networking.wg-quick.interfaces = {
-      proton-DK25 = {
+      proton-wg = {
         autostart = false;
         address = [ "10.2.0.2/32" ];
         dns = [ "10.2.0.1" ];
         privateKeyFile = config.age.secrets.wireguard.path;
         peers = [
           {
-            publicKey = "sbjnjFtxUz4dxYfNL7WOVf1StMjjAhkiPLCPtVtlhRI=";
+            publicKey = "XPVCz7LndzqWe7y3+WSo51hvNOX8nX5CTwVTWhzg8g8=";
             allowedIPs = [ "0.0.0.0/0" ];
-            endpoint = "193.29.107.162:51820";
+            endpoint = "149.88.27.234:51820";
           }
         ];
       };
